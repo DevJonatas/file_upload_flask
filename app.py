694333ac65a71c2fbbda4059d7dd399d174a1ec0
DevJonatas <jonatas@workmail.com>
2019-04-19 11:41:56 -0300

    #commit road upload using app.config and using name of archive with name name chose user

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, redirect
+import os
+
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
+
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/save_image', methods=['POST',])
+def save_image():
+    name = request.form['name']
+    archive = request.files['image']
+
+    extension = os.path.splitext(archive.filename)[1]
+    f_name = str(name + extension)
+
+    upload_path = app.config['UPLOAD_PATH']
+
+    archive.save(os.path.join(upload_path, f_name))
+
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True,port=5380)
